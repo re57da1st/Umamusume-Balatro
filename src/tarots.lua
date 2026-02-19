0fe2dc1efc7b4f5c7c70aef38f0b98044f7d864c
re57da1st <re57da1st@gmail.com>
2026-02-18 18:04:13 -0600

    sdfsdfsdfsdf

--- src/tarots.lua
+++ src/tarots.lua
@@ -17,42 +17,48 @@ SMODS.ConsumableType {
 }
 
 --BETTER TAROTS
+--Check compatibility with Fortune Teller
 
---Ts ain't even started yet
+--Joel do thing >.<
 SMODS.Consumable { -- The Fool
     key = 'better_fool',
     set = 'uma_better_Tarot',
     pos = { x = 0, y = 0 },
     atlas = 'c_umas',
     loc_vars = function(self, info_queue, card)
-        -- This vanilla variable only checks for vanilla Tarots and Planets, you would have to keep track on your own for any custom consumables
-        local fool_c = G.GAME.last_tarot_planet and G.P_CENTERS[G.GAME.last_tarot_planet] or nil
-        local last_tarot_planet = fool_c and localize { type = 'name_text', key = fool_c.key, set = fool_c.set } or
-            localize('k_none')
-        local colour = (not fool_c or fool_c.name == 'The Fool') and G.C.RED or G.C.GREEN
-
-        if not (not fool_c or fool_c.name == 'The Fool') then
-            info_queue[#info_queue + 1] = fool_c
-        end
-
+        local uma_tarots = G.GAME.uma_tarot_card
+        local uma_planets = G.GAME.uma_planet_card
+        local uma_tarots_named = uma_tarots and localize({type = 'name_text', key = uma_tarots, set = G.P_CENTERS[uma_tarots].set}) or localize('k_none')
+        local uma_planets_named = uma_planets and localize({type = 'name_text', key = uma_planets, set = G.P_CENTERS[uma_planets].set}) or localize('k_none')
+        local colour_tarots = uma_tarots and G.C.GREEN or G.C.RED
+        local colour_planets = uma_planets and G.C.GREEN or G.C.RED
         local main_end = {
             {
                 n = G.UIT.C,
                 config = { align = "bm", padding = 0.02 },
                 nodes = {
                     {
-                        n = G.UIT.C,
-                        config = { align = "m", colour = colour, r = 0.05, padding = 0.05 },
+                        n = G.UIT.R,
+                        config = { align = "m", colour = colour_tarots, r = 0.05, padding = 0.05 },
                         nodes = {
-                            { n = G.UIT.T, config = { text = ' ' .. last_tarot_planet .. ' ', colour = G.C.UI.TEXT_LIGHT, scale = 0.3, shadow = true } },
+                            { n = G.UIT.T, config = { text = ' ' .. uma_tarots_named .. ' ', colour = G.C.UI.TEXT_LIGHT, scale = 0.3, shadow = true } },
+                        }
+                    },
+                    {
+                        n = G.UIT.R,
+                        config = { align = "m", colour = colour_planets, r = 0.05, padding = 0.05 },
+                        nodes = {
+                            { n = G.UIT.T, config = { text = ' ' .. uma_planets_named .. ' ', colour = G.C.UI.TEXT_LIGHT, scale = 0.3, shadow = true } },
                         }
                     }
                 }
             }
         }
 
-        return { vars = { last_tarot_planet }, main_end = main_end }
+        return { main_end = main_end }
+        
     end,
+
     use = function(self, card, area, copier)
         G.E_MANAGER:add_event(Event({
             trigger = 'after',
