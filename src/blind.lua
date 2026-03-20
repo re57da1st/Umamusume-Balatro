151b25595d90a9610cf612338a5a72d381f72e97
FriskScyobi <kyojuromybaby@gmail.com>
2026-03-20 04:45:50 -0400

    Update blind.lua

--- src/blind.lua
+++ src/blind.lua
@@ -9,7 +9,7 @@ SMODS.Blind {
     calculate = function(self, blind, context)
         if not blind.disabled then
             if context.hand_drawn and not context.first_hand_drawn then
-                G.GAME.blind.chips = 50
+                G.GAME.blind.chips = G.GAME.blind.chips * 1.25
                 G.GAME.blind.chip_text = number_format(G.GAME.blind.chips)
             end
         end
