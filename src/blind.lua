ee037cfe506f2671ae12af615edefa172cdd32cf
FriskScyobi <kyojuromybaby@gmail.com>
2026-03-20 04:02:27 -0400

    Update blind.lua

--- src/blind.lua
+++ src/blind.lua
@@ -8,25 +8,10 @@ SMODS.Blind {
     boss_colour = HEX("009cfd"),
     calculate = function(self, blind, context)
         if not blind.disabled then
-            if context.hand_drawn then
-                local any_forced = nil
-                for _, playing_card in ipairs(G.hand.cards) do
-                    if playing_card.ability.forced_selection then
-                        any_forced = true
-                    end
-                end
-                if not any_forced then
-                    G.hand:unhighlight_all()
-                    local forced_card = pseudorandom_element(G.hand.cards, 'vremade_cerulean_bell')
-                    forced_card.ability.forced_selection = true
-                    G.hand:add_to_highlighted(forced_card)
-                end
+            if context.hand_drawn and not context.first_hand_drawn then
+                G.GAME.blind.chips = 50
             end
         end
     end,
-    disable = function(self)
-        for _, playing_card in ipairs(G.playing_cards) do
-            playing_card.ability.forced_selection = nil
-        end
-    end
+
 }
