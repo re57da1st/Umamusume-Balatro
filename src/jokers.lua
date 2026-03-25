33651c50b69c374c0e903f1e9aad89497f6f9987
FriskScyobi <kyojuromybaby@gmail.com>
2026-03-25 04:25:20 -0400

    Update jokers.lua

--- src/jokers.lua
+++ src/jokers.lua
@@ -1597,10 +1597,10 @@ SMODS.Joker{ --Maruzensky
     end,
 
         add_to_deck = function(self, card, from_debuff)
-        G.GAME.round_resets.hands = card.ability.extra.hands
+        ease_hands_played(card.ability.extra.hands)
     end,
     remove_from_deck = function(self, card, from_debuff)
-        G.GAME.round_resets.hands = G.GAME.round_resets.hands
+        return nil
     end,
     
     calculate = function(self, card, context)
